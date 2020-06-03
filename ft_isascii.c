c4f1e55a2885f11d5fbf7b6bfb7d2fea359f940b
Sphelele Shandu <sphiwokuhleshandu24@gmail.com>
2020-06-03 18:05:38 +0200

    ft_isascii, not sure about it yet

---
+++ ft_isascii.c
@@ -0,0 +1,9 @@
+
+//not sure about this one, shall double check when i get back to camp
+int     ft_isascii(int c)
+{
+    if (c >= 32 && c <= 126)
+        return (1);
+    else 
+        return (0);
+}
