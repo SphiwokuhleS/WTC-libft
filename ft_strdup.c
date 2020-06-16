6aaf7da8733747e21e59de6c082a364494b423e0
sph3kTor <sphe@localhost.localdomain>
2020-06-16 10:11:34 +0200

    strdup and strcpy

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+char    *ft_strdup(const char *s)
+{
+
+    int i; //counter
+    char *dup_str;
+
+    i = 0;
+    //count the number of characters in the original string
+    while (s[i] != '\0') //could also use my ft_strlen()
+    {
+        i++;
+    }
+
+    dup_str = (char *)malloc(i * sizeof(char) + 1); //assign enough memory for the duplicate string
+    if (dup_str == 0) //if no memory was assigned to dup_str, just return 
+    {
+        return (dup_str);
+    }
+
+    while (i >= 0)
+    {
+        dup_str[i] = s[i]; //duplication process
+        i--;
+    }
+    dup_str[i] = '\0'; //string must end with '\0'
+    return(dup_str);
+    free(dup_str); //free the memory
+}
